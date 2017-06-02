467aa4164a43244cc512ccab412f6ea417bdf03e
forevermatt <forevermatt@users.noreply.github.com>
2017-06-01 21:16:52 -0400

    Get basic routing working as desired.

--- js/budget-app.js
+++ js/budget-app.js
@@ -1,6 +1,30 @@
 var bb = bb || {};
 
-bb.BudgetApp = function() {
+bb.BudgetApp = function(pageDom) {
   this.budget = new bb.Budget();
-  this.page = new bb.Page();
+  this.page = new bb.Page(pageDom);
+  this.routes = {
+    'budget': {
+      'tagName': 'bb-budget',
+      'opts': {
+        'categories': this.budget.categories,
+        'buttons': this.page.buttons
+      }
+    },
+    'expense/account': {
+      'tagName': 'bb-expense-account',
+      'opts': {}
+    }
+  };
+  route(this.route.bind(this));
+};
+
+bb.BudgetApp.prototype.route = function(...pathSegments) {
+  var path = pathSegments.join('/');
+  var routeData = this.routes[path];
+  if (routeData != undefined) {
+    this.page.showTag(routeData.tagName, routeData.opts);
+  } else {
+    route('budget');
+  }
 };
