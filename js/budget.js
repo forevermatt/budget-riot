418c07aedafe5e3c1726fdd99671de87eaf264aa
forevermatt <forevermatt@users.noreply.github.com>
2017-06-03 10:16:49 -0400

    GIve the budget a list of accounts.

--- js/budget.js
+++ js/budget.js
@@ -1,6 +1,15 @@
 var bb = bb || {};
 
 bb.Budget = function() {
+  this.accounts = [{
+    'name': '23: checking'
+  }, {
+    'name': '71: credit card'
+  }, {
+    'name': '85: other credit card'
+  }, {
+    'name': 'cash'
+  }];
   this.categories = [
     new bb.Category('Auto', 298,  400),
     new bb.Category('Food', 70, 300),
