6972073cb7edcf1ba364919542b1f8cd8cbd2805
forevermatt <forevermatt@users.noreply.github.com>
2017-06-15 19:26:44 -0400

    Add Transaction class.

---
+++ js/transaction.js
@@ -0,0 +1,35 @@
+var bb = bb || {};
+
+bb.Transaction = function(config) {
+
+  config = config || {};
+
+  /* NOTE: Amounts are always in integers, counting the smallest unit of the *
+   *       currency (such as cents, if the currency is USD).  */
+  this.accountId = config.accountId;
+  this.amountTotal = config.amountTotal;
+  this.categories = config.categories || [];
+  this.whenTimestamp = config.whenTimestamp;
+  this.who = config.who;
+};
+
+bb.Transaction.format = function(amount) {
+  return (Number(amount) / 100).toFixed(2);
+};
+
+bb.Transaction.prototype.isValid = function() {
+  return this.accountId &&
+         this.amountTotal &&
+         this.categories.length &&
+         this.whenTimestamp &&
+         this.who &&
+         (this.sumCategories() === this.amountTotal);
+};
+
+bb.Transaction.prototype.sumCategories = function() {
+  var tempTotal = 0;
+  for (var i = 0; i < this.categories.length; i++) {
+    tempTotal += this.categories[i].amount;
+  }
+  return tempTotal;
+};
