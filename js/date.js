fda3df2d6080ee15ad0bdad7f6386569f6de0893
forevermatt <forevermatt@users.noreply.github.com>
2017-06-17 12:53:05 -0400

    Add function for getting short date string.

--- js/date.js
+++ js/date.js
@@ -6,3 +6,8 @@ bb.Date = function() {
 bb.Date.format = function(timestamp) {
   return (new Date(timestamp)).toLocaleDateString();
 };
+
+bb.Date.short = function(timestamp) {
+  var date = new Date(timestamp);
+  return (date.getMonth() + 1) + '/' + date.getDate();
+};
